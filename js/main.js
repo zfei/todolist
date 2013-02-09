5cbb532646c7ca94efd91751f6ff348fd3bfd2e9
Zhiye Fei <1736058+zfei@users.noreply.github.com>
2013-02-09 16:38:20 -0600

    Now able to read from local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,70 @@
 $(document).ready(function() {
 
+	var getLists = function() {
+		var lists = localStorage.getItem("lists");
+		if(lists === null) {
+			localStorage.setItem("lists", "[]");
+			lists = "[]";
+		}
+		return JSON.parse(lists);
+	}
+
+	var getTasks = function() {
+		var lists = getLists();
+		if(lists === []) {
+			return [];
+		}
+		var list_name = $('#list .selected').html();
+		if(list_name === "") {
+			return [];
+		}
+		var tasks = [];
+		for(var i = 0; i < lists.length; i++) {
+			if(lists[i].list_name === list_name) {
+				tasks = lists[i].tasks;
+				break;
+			}
+		}
+		return tasks;
+	};
+
+	var showLists = function() {
+		var lists = getLists();
+		$('#list').empty();
+		$('#list').append('<li id="new_list">New List</li>');
+		if(lists === []) {
+			return;
+		}
+		for(var i = 0; i < lists.length; i++) {
+			if(i === 0) {
+				$('#list').append('<li class="selected">' + lists[i].list_name + '</li>');
+			} else {
+				$('#list').append('<li>' + lists[i].list_name + '</li>');
+			}
+		}
+	}
+
+	var showTasks = function() {
+		var tasks = getTasks();
+		$('#tasks').empty();
+		$('#tasks').append('<li id="new_task">New Task</li>');
+		$('#dones').empty();
+		$('#dones').append('<li class="label">Done</li>');
+		$('#removes').empty();
+		$('#removes').append('<li class="label">Remove</li>');
+		for(var i = 0; i < tasks.length; i++) {
+			var task_name = tasks[i].task_name;
+			var done = tasks[i].done;
+			$('#tasks').append('<li class="task' + i + '">' + task_name + '</li>');
+			$('#dones').append('<li class="task' + i + '"><img class="undone" src="./img/icon/' + done + '.png"></li>');
+			$('#removes').append('<li class="task' + i + '"><img class="remove" src="./img/icon/del.png"></li>');
+		}
+	}
+
+	showLists();
+
+	showTasks();
+
 	$('#new_list').click(function() {
 		var list_name = prompt("Please enter list name", "My List");
 		if(list_name != null && list_name != "") {
@@ -11,12 +76,21 @@ $(document).ready(function() {
 		var task_name = prompt("Please enter task name", "My task");
 		if(task_name != null && task_name != "") {
 			$('#tasks').append('<li class="task task">' + task_name + '</li>');
-			$('#dones').append('<li class="status task"><img class="undone" src="./img/icon/undone.png"></li>');
-			$('#removes').append('<li class="remove task"><img class="remove" src="./img/icon/del.png"></li>');
+			$('#dones').append('<li class="task"><img class="undone" src="./img/icon/undone.png"></li>');
+			$('#removes').append('<li class="task"><img class="remove" src="./img/icon/del.png"></li>');
 		}
 	});
 
-	$(document).on("click", ".status", function(){
+	$(document).on("click", "#list li", function() {
+		if($(this).hasClass("selected") || $(this).attr("id") === "new_list"){
+			return;
+		}
+		$('.selected').removeClass("selected");
+		$(this).addClass("selected");
+		showTasks();
+	});
+
+	$(document).on("click", "#dones li", function() {
 		if($(this).children().hasClass("done")) {
 			$(this).children().removeClass("done");
 			$(this).children().addClass("undone");
@@ -28,11 +102,10 @@ $(document).ready(function() {
 		}
 	});
 
-	$(document).on("click", "li.remove", function(){
+	$(document).on("click", "#removes li", function() {
 		var $remove_class = $(this).attr("class");
-		$remove_class = $remove_class.split(" ")[1];
-		$('.'+$remove_class).hide("slow", function(){
-			$('.'+$remove_class).remove();
+		$('.' + $remove_class).hide("slow", function() {
+			$('.' + $remove_class).remove();
 		});
 	});
 
